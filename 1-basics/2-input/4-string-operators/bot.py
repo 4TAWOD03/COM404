6153207a708b14ce48a48866291ab3544183f57c
4TAWOD03 <4tawod03@SOLENT.AC.UK>
2019-10-18 11:55:28 +0000

    added code to demonstrate the use of sting operators.

---
+++ 1-basics/2-input/4-string-operators/bot.py
@@ -0,0 +1,12 @@
+print("Please enter the number of lives.")
+lives = int(input())
+print("Please enter the energy level.")
+energy = int(input())
+print("Please enter the shield level.")
+shield = int(input())
+
+print("health has been set")
+
+print("lives: ","♥"*lives)
+print("energy:","♦"*energy)
+print("shield:","♦"*shield)
